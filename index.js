698a5ea0323036f6718d46dcd4676db1cb3d0603
Emmanuel Etti <emmanueletti@gmail.com>
2021-07-02 22:26:48 +0000

    finished assignment

--- index.js
+++ index.js
@@ -1,29 +1,59 @@
-let balance = 500.00;
+class Account {
+  constructor(username) {
+    this._username = username;
+    this._balance = 0;
+    this._transactions = [];
+  }
 
-class Withdrawal {
+  getBalance() {
+    return this._balance;
+  }
 
-  constructor(amount) {
+  addTransaction(transaction) {
+    this._transactions.push(transaction);
+  }
+}
+
+// super
+class Transaction {
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
 
   commit() {
-    balance -= this.amount;
+    this.time = new Date();
+    this.account.addTransaction(this);
+    this.account._balance += this.getValue();
   }
-
 }
 
+// sub class
+class Withdrawal extends Transaction {
+  getValue() {
+    return -this.amount;
+  }
+}
 
-
+// sub class
+class Deposit extends Transaction {
+  getValue() {
+    return this.amount;
+  }
+}
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+const myAccount = new Account("billybob");
+
+console.log("Starting Balance:", myAccount._balance);
+
+const t1 = new Deposit(120.0, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+const t2 = new Withdrawal(50.0, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+console.log("Ending Balance:", myAccount._balance);
+console.log("Transactions:", myAccount._transactions);
